55bfb3de15b91adc5950e75f78df00d0440a4a1c
Praditya <muhammadpraditya333@gmail.com>
2024-03-13 12:22:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/p6_androidroomwithaview/WordDao.java
@@ -0,0 +1,21 @@
+package com.example.p6_androidroomwithaview;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+}
